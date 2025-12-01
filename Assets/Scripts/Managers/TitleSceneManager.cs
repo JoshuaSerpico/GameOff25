a9cb1acad46026c523fbc53abb44a80840e60d8d
Ryan <ryan.provenz@gmail.com>
2025-12-01 11:18:32 -0500

    titlescreen created + scene order for building

---
+++ Assets/Scripts/Managers/TitleSceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleSceneManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
